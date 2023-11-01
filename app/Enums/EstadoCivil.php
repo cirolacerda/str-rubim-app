a99f8bdecd93405360cd48b96c13d43f64fce7e4
Ciro Lacerda <cirolacerda@gmail.com>
2023-11-01 20:51:29 -0300

    Criando enum estado civil

---
+++ app/Enums/EstadoCivil.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+enum EstadoCivil: string
+{
+
+case casado_civil = 'Casado no Civil';
+case casado_religioso = 'Casado no Religioso';
+case solteiro = 'Solteiro';
+case viuvo = 'Viúvo';
+case divorciado = 'Divorciado';
+case desquitado = 'Desquitado';
+
+}
+
+
+?>
